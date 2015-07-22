fb77421ef82aad0aef94411faf4b76e8e0528777
ddc <diaz_carrete@yahoo.com>
2015-07-22 21:51:19 +0200

    IsString.

--- library/Data/Dani/Types.hs
+++ library/Data/Dani/Types.hs
@@ -1,21 +1,35 @@
 {-# LANGUAGE DeriveFunctor #-}
 
 module Data.Dani.Types (
-        DnF(..)
+        Symbol
+    ,   symbol
+    ,   DnF(..)
     ,   Dn
     ,   Dn_
     ) where
 
 import qualified Data.Text as T
 import qualified Data.Scientific as N
+import Data.String
 import Control.Comonad.Trans.Cofree
 import Control.Comonad.Env
 
-data DnF a = Symbol T.Text 
+newtype Symbol = MakeSymbol T.Text deriving (Show, Eq)
+
+instance IsString Symbol where
+    fromString = MakeSymbol . T.pack
+
+symbol :: T.Text -> Symbol 
+symbol = MakeSymbol
+
+data DnF a = Symbol Symbol
            | String T.Text 
            | Number N.Scientific
            | List [a]
            deriving (Show,Eq,Functor)
+
+instance IsString (DnF a) where
+    fromString = String . T.pack
 
 type Dn = CofreeT DnF (Env [Dn_]) ()
 
