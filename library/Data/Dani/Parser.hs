e6d5d5151616212cc87488e91e0da737c5c1c29f
ddc <diaz_carrete@yahoo.com>
2015-07-24 17:42:05 +0200

    simple parsers

--- library/Data/Dani/Parser.hs
+++ library/Data/Dani/Parser.hs
@@ -1,4 +1,25 @@
 module Data.Dani.Parser (
+        module Data.Dani.Types
     ) where
 
+import qualified Data.Attoparsec.Text as P
+import qualified Data.Text as T
+import Data.Char
+
 import Data.Dani.Types
+
+symbolParser :: P.Parser Symbol  
+symbolParser = symbol <$> P.takeWhile1 isAlpha  
+
+stringParser :: P.Parser T.Text
+stringParser = P.char '"' *> P.takeWhile1 (\c -> not (c=='"'))  <* P.char '"' 
+
+skipSpaceAndComma :: P.Parser ()
+skipSpaceAndComma = P.skipWhile (\c -> isSpace c || c==',')
+
+elementParser :: P.Parser Dn
+elementParser = undefined
+
+listParser :: P.Parser [Dn] 
+listParser = P.char '(' *> undefined 
+
