b05436dd53959771d8ef11f7ec4e8ece77a9d424
ddc <diaz_carrete@yahoo.com>
2015-07-28 09:33:01 +0200

    working on the builders

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # dani-dn
-Dani's Data Notation: s-expressions with a few bells and whistles.
+Dani's Data Notation: simple s-expressions with a few bells and whistles.
+
+See also the [atto-lisp](https://hackage.haskell.org/package/atto-lisp) and [hedn](https://hackage.haskell.org/package/hedn) packages.
--- library/Data/Dani/Encode.hs
+++ library/Data/Dani/Encode.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ViewPatterns #-}
 
 module Data.Dani.Encode (
         module Data.Dani
@@ -6,11 +7,28 @@ module Data.Dani.Encode (
 
 
 import Data.Dani
+import Data.Monoid
 import Data.Text.Lazy.Builder.Scientific
 import qualified Data.Text.Lazy.Builder as T
+import Control.Comonad.Trans.Cofree
+
+space :: T.Builder
+space = T.singleton ' '
+
+meta :: T.Builder
+meta = T.singleton '^'
+
+lparen :: T.Builder  
+lparen = T.singleton '('
+
+rparen :: T.Builder  
+rparen = T.singleton '('
 
 encodeToTextBuilder :: Value -> T.Builder
 encodeToTextBuilder = undefined
 
 encodeToTextBuilder_ :: Value_ -> T.Builder
-encodeToTextBuilder_ = undefined
+encodeToTextBuilder_ v_ = case unwrap v_ of 
+    String s -> T.fromText s
+    Number n -> scientificBuilder n 
+    List vs -> lparen  <> _ <> rparen  
