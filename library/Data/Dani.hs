1e8951b52ae1c232a93bb81668a56f8fb7e8ace3
ddc <diaz_carrete@yahoo.com>
2015-07-30 00:27:32 +0200

    CPP for orphan ComonadHoist instance.

--- library/Data/Dani.hs
+++ library/Data/Dani.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP #-}
 module Data.Dani 
     ( 
         module Data.Dani.Types
@@ -7,8 +8,12 @@ module Data.Dani
 
 import Data.Dani.Types
 
+#if !(MIN_VERSION_free(4,12,2))
+import Data.Bifunctor
+#endif
 import Control.Comonad.Trans.Cofree
 import Control.Comonad.Env
+import Control.Comonad.Hoist.Class
 
 value :: a 
       -> [Cofree ValueF b] 
@@ -19,9 +24,8 @@ value a e = CofreeT . env e . (:<) a
 value_ :: a -> ValueF (Cofree ValueF a) -> Cofree ValueF a
 value_ a = cofree . (:<) a
 
--- askMeta :: CofreeT ValueF (Env [Cofree ValueF b]) a
---         -> [Cofree ValueF b]
--- askMeta = ask . lower
-
-
+#if !(MIN_VERSION_free(4,12,2))
+instance Functor f => ComonadHoist (CofreeT f) where
+    cohoist g = CofreeT . fmap (second (cohoist g)) . g . runCofreeT
+#endif
 
